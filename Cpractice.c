60f1dcf7efd57395f6c964261fa799d6cf3b9115
jimmy404 <bruno_pier@hotmail.com>
2020-10-17 00:40:45 -0300

    Program that multiplies from 1 to 9

--- Cpractice.c
+++ Cpractice.c
@@ -1,24 +1,17 @@
-//Program that reads two numbers and shows the numbers between the declared numbers.
+//Program that multiplies from 1 to 9.
 
 #include <stdio.h>
 
 int main() {
 	
-	int x, y, i;
+	int i, j;
 	
-	printf("Enter a number: ");
-	scanf("%i",&x);
-	
-	do{
-		printf("Enter a number greater than the previous one: ");
-		scanf("%i",&y);
-	}while (y < x);
-
-	for(i = x + 1; i < y; i++){
-		printf("%i ",i);
+	for(i = 1; i <= 9; i++){
+		for(j = 1; j <= 10; j++){
+			printf("%i x %i = %i.\n",i,j,i*j);
+		}
 	}
-	printf("\n");
-	
+
 	system("pause");
 	return 0;
 }
