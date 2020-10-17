c7f12f161d5172efc2f1dfbe7d6835af8288883d
jimmy404 <bruno_pier@hotmail.com>
2020-10-17 00:22:06 -0300

    Adds exercise with - do while & fflush

--- Cpractice.c
+++ Cpractice.c
@@ -1,1 +1,16 @@
+//fflush exercise
 
+#include <stdio.h>
+
+int main () {
+	char c;
+	
+	do{
+		printf("Introduce una letra: ");
+		fflush(stdin);
+		scanf("%c",&c);
+	}while(c != 's' && c != 'S');
+	
+	system("pause");
+	return 0;
+}
