4c59f40bedb073f2ba3bc4c735c0efe2de3d839c
jimmy404 <bruno_pier@hotmail.com>
2020-10-18 00:16:52 -0300

    Program - Fibonacci sequence

--- Cpractice.c
+++ Cpractice.c
@@ -1,24 +1,33 @@
-//Program - Factorial of a number / Code debugging.
+//Program - Program - Fibonacci sequence.
 
 #include <stdio.h>
 
 int main() {
 	
-	int x, i, fact;
+	int x, y, res, n, i;
 	
-	fact = 1;
+	printf ("Enter a number for the Fibonacci sequence: ");
+	scanf ("%i",&n);
 	
-	printf("Add a number to calculate the factorial: ");
-	scanf("%i",&x);
+	i = 3;
+	x = 0;
+	y = 1;
 	
-	for(i = 1; i <= x; i++){
-		fact = fact * i;
+	if(n == 1){
+		printf("0 ");
+	}else{
+		printf("0, 1, ");
 	}
 	
-	printf("The factorial of %i is %i.\n",x,fact);
-
-
-	system("pause");
+	while (i <= n){
+		res = x + y;
+		printf("%i, ",res);
+		x = y;
+		y = res;
+		i++;
+	}
+	
+	system ("pause");
 	return 0;
-
 }
+
