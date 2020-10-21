0546cb6061b1a0daf1c912df93d985a1f07d253b
jimmy404 <bruno_pier@hotmail.com>
2020-10-21 01:05:09 -0300

    Program - Create a matrix and read it on the screen

--- Cpractice.c
+++ Cpractice.c
@@ -1,64 +1,40 @@
-//Program - "Calculate the probabilities that each of the numbers comes out with a vector using pointers."
+//Program - "Create a matrix and read it on the screen."
 
 #include <stdio.h>
-void roll(int *v);
-void read(int *v);
+void addElements(int matrix[][5]);
+void readElements(int matrix[][5]);
 
 int main(){
 	
-	int v[] = {0,0,0,0,0,0};
+	int matrix[5][5];
 	
-	roll(v);
-	read(v);
+	addElements(matrix);
+	readElements(matrix);
 
 	system("pause");
 	return 0;
 }
 
-void roll(int *v){
+void addElements(int matrix[][5]){
+	int i, j;
 	srand(time(NULL));
-	int i, aux;
 	
-	for( i = 0; i < 10000; i++){
-		aux = rand()%6;
-		
-		switch(aux){
-			case 0: {
-				*(v+aux) += 1;
-				break;
-			}
-			case 1: {
-				*(v+aux) += 1;
-				break;
-			}
-			case 2: {
-				*(v+aux) += 1;
-				break;
-			}
-			case 3: {
-				*(v+aux) += 1;
-				break;
-			}
-			case 4: {
-				*(v+aux) += 1;
-				break;
-			}
-			case 5: {
-				*(v+aux) += 1;
-				break;
-			}
+	for(i = 0; i < 5; i++){
+		for(j = 0; j < 5; j++){
+			matrix[i][j] = rand()%10;
 		}
 	}
 }
 
-void read(int *v){
-	int i;
-	float x;
+void readElements(int matrix[][5]){
+	int i, j;
+	srand(time(NULL));
 	
-	
-	for(i = 0; i < 6; i++){
-		x = (*(v+i)*100)/10000.;
-		printf("The probability that the number %i comes out is %f.\n:",i+1,x);
+	for(i = 0; i < 5; i++){
+		for(j = 0; j < 5; j++){
+			printf("%i ",matrix[i][j]);
+		}
+		printf("\n");
 	}
 }
 
