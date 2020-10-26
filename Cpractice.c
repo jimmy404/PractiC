43ef71496d670c63b8887b46d42bc9dcd86a6ee4
jimmy404 <bruno_pier@hotmail.com>
2020-10-25 23:40:17 -0300

    Program - Structures - Program that prints information from a book.

--- Cpractice.c
+++ Cpractice.c
@@ -1,26 +1,42 @@
-//Program - "Compare two character strings."
+//Program - Structures - Program that prints books - basic.
 
 #include <stdio.h>
 #include <string.h>
 
+typedef struct{
+	int pages;
+	char name[50];
+	float price;
+}book;
+
 int main(){
 	
-	char ch1[7];
-	char ch2[7];
+	book book1, book2, book3;
 	
-	printf("First sentence: ");
-	fgets(ch1,7,stdin);
+	book1.pages = 350;
+	strcpy(book1.name, "First structure");
+	book1.price = 20.2;
 	
-	printf("Second sentence: ");
-	fflush(stdin);
-	fgets(ch2,7,stdin);
+	book2.pages = 450;
+	strcpy(book2.name, "Second structure");
+	book2.price = 18.2;
 	
-	if(strcmp(ch1,ch2) == 0){
-		printf("The two sentences are the same.\n");
-	}else{
-		printf("The two sentences are NOT the same.\n\n");
-	}
+	book3.pages = 550;
+	strcpy(book3.name, "Third structure");
+	book3.price = 24.2;
 
+	
+	printf("The book 1 has %i pages.\n",book1.pages);
+	printf("The book 1 is called %s .\n",book1.name);
+	printf("The price of the book 1 is $ %.2f.\n\n",book1.price);
+	
+	printf("The book 2 has %i pages.\n",book2.pages);
+	printf("The book 2 is called %s .\n",book2.name);
+	printf("The price of the book 2 is $%.2f.\n\n",book2.price);
+	
+	printf("The book 3 has %i pages.\n",book3.pages);
+	printf("The book 3 is called %s .\n",book3.name);
+	printf("The price of the book 3 is $%.2f.\n\n",book3.price);
 
 	system("pause");
 	return 0;
