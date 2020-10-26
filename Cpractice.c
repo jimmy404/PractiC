ae12473a14d710592db5a65b7e5db44d76acb4b2
jimmy404 <bruno_pier@hotmail.com>
2020-10-26 01:03:18 -0300

    Program - Structures - Gives value to 10 different structures through a for.

--- Cpractice.c
+++ Cpractice.c
@@ -1,4 +1,4 @@
-//Program - Structures - Program that prints books - basic.
+//Program - Structures - Gives value to 10 different structures through a for.
 
 #include <stdio.h>
 #include <string.h>
@@ -11,32 +11,13 @@ typedef struct{
 
 int main(){
 	
-	book book1, book2, book3;
+	int i;
+	book book[10];
 	
-	book1.pages = 350;
-	strcpy(book1.name, "First structure");
-	book1.price = 20.2;
-	
-	book2.pages = 450;
-	strcpy(book2.name, "Second structure");
-	book2.price = 18.2;
-	
-	book3.pages = 550;
-	strcpy(book3.name, "Third structure");
-	book3.price = 24.2;
-
-	
-	printf("The book 1 has %i pages.\n",book1.pages);
-	printf("The book 1 is called %s .\n",book1.name);
-	printf("The price of the book 1 is $ %.2f.\n\n",book1.price);
-	
-	printf("The book 2 has %i pages.\n",book2.pages);
-	printf("The book 2 is called %s .\n",book2.name);
-	printf("The price of the book 2 is $%.2f.\n\n",book2.price);
-	
-	printf("The book 3 has %i pages.\n",book3.pages);
-	printf("The book 3 is called %s .\n",book3.name);
-	printf("The price of the book 3 is $%.2f.\n\n",book3.price);
+	for (i = 0; i < 10; i++){
+		book[i].price = 20 + i;
+		printf("The price of the structure %i is %f.\n",i+1,book[i].price);
+	}
 
 	system("pause");
 	return 0;
